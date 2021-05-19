cd9b3773ccee9ad5edaa558586d8ba182c39511d
Andoni <andoni0305@gmail.com>
2021-05-19 17:30:16 +1200

    Adding new colorscheme and small changes

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,9 +1,17 @@
 vim.cmd('let g:nvcode_termcolors=256')
 
+vim.g.moonlight_italic_comments = true
+vim.g.moonlight_italic_keywords = true
+-- vim.g.moonlight_italic_functions = true
+-- vim.g.moonlight_italic_variables = true
+vim.g.moonlight_contrast = true
+vim.g.moonlight_borders = true
+vim.g.moonlight_disable_background = false
+
 vim.cmd('colorscheme ' .. O.colorscheme)
 -- background transparency
 if O.transparent_bg then
-	vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
-	vim.cmd('hi LineNr ctermbg=NONE guibg=NONE')
-	vim.cmd('hi SignColumn ctermbg=NONE guibg=NONE')
+  vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
+  vim.cmd('hi LineNr ctermbg=NONE guibg=NONE')
+  vim.cmd('hi SignColumn ctermbg=NONE guibg=NONE')
 end
