f0cc5588e83b6077c51a3d6fe66c8abb3d9e1b45
Andoni <andoni0305@gmail.com>
2021-05-25 14:56:04 +1200

    Minor change

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -4,7 +4,7 @@ vim.g.moonlight_italic_comments = true
 vim.g.moonlight_italic_keywords = true
 -- vim.g.moonlight_italic_functions = true
 -- vim.g.moonlight_italic_variables = true
-vim.g.moonlight_contrast = false
+vim.g.moonlight_contrast = true
 vim.g.moonlight_borders = true
 vim.g.moonlight_disable_background = false
 
