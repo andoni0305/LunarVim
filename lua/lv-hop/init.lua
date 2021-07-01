4a704d90b746a2d7cb9d9f531a3729a047c0b63b
Andoni <andoni0305@gmail.com>
2021-07-02 10:25:14 +1200

    hop finally works with visual extend

--- lua/lv-hop/init.lua
+++ lua/lv-hop/init.lua
@@ -2,7 +2,10 @@ local M = {}
 
 M.config = function()
     require('hop').setup({})
-    vim.api.nvim_set_keymap('n', 's', ":HopWord<cr>", {silent = true})
+    vim.api.nvim_set_keymap('n', 's', "<cmd>lua require'hop'.hint_words()<cr>",
+                            {silent = true})
+    vim.api.nvim_set_keymap('v', 's', "<cmd>lua require'hop'.hint_words()<cr>",
+                            {silent = true})
 end
 
 return M
