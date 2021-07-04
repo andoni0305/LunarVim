4020d7dc0c9062cf5cb9ba3c26ef64f786efc86c
Andoni <andoni0305@gmail.com>
2021-07-04 16:14:27 +1200

    cleanup

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,9 +1,2 @@
 -- TODO is there a lua native way to do this?
 vim.cmd('colorscheme ' .. O.colorscheme)
-
--- background transparency
-if O.transparent_bg then
-  vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
-  vim.cmd('hi LineNr ctermbg=NONE guibg=NONE')
-  vim.cmd('hi SignColumn ctermbg=NONE guibg=NONE')
-end
