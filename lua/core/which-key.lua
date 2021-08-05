5f577590491ea8ded56e29ee696cb77b35299df1
Andoni <andoni@thankyoupayroll.co.nz>
2021-08-06 10:33:22 +1200

    Moving stuff into config file

--- lua/core/which-key.lua
+++ lua/core/which-key.lua
@@ -192,14 +192,6 @@ M.config = function()
         name = "Treesitter",
         i = { ":TSConfigInfo<cr>", "Info" },
       },
-      r = {
-        name = "Replace",
-        f = {
-          "<cmd>lua require('spectre').open_file_search()<cr>",
-          "Current File",
-        },
-        p = { "<cmd>lua require('spectre').open()<cr>", "Project" },
-      },
     },
   }
 end
