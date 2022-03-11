fb25bed53981838ef64000cd96a1e257f3c2202f
Andoni Arbulu Lozano <ando.arbulu@sailthru.com>
2022-03-11 15:25:21 +1300

    chore(plugin-loader): going down to 10 max_jobs because of osx issues =(

--- lua/lvim/plugin-loader.lua
+++ lua/lvim/plugin-loader.lua
@@ -35,7 +35,7 @@ function plugin_loader.init(opts)
         fetch = "fetch --no-tags --no-recurse-submodules --update-shallow --progress",
       },
     },
-    max_jobs = 50,
+    max_jobs = 10,
     display = {
       open_fn = function()
         return require("packer.util").float { border = "rounded" }
