686c0e77279e6f3af9789f1760219084f27666d5
Andoni Arbulu Lozano <ando.arbulu@sailthru.com>
2022-03-16 08:52:26 +1300

    chore(treesitter): gotta go slow; osx issues

--- lua/lvim/core/treesitter.lua
+++ lua/lvim/core/treesitter.lua
@@ -6,6 +6,7 @@ M.config = function()
     on_config_done = nil,
     ensure_installed = {}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     ignore_install = {},
+    sync_install = true,
     matchup = {
       enable = true, -- mandatory, false will disable the whole extension
       -- disable = { "c", "ruby" },  -- optional, list of language that will be disabled
